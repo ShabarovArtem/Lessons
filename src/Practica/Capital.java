4c0d87ae7f9f5f37c5c7866e0398303a19e34089
ShabarovArtem <shabarovartemy@yandex.ru>
2024-02-10 23:44:47 +0300

    Init

---
+++ src/Practica/Capital.java
@@ -0,0 +1,11 @@
+package Practica;
+
+public class Capital {
+    public static void main(String[] args) {
+        char[] answer = {'Д','о','х','а'};
+//        answer[0] = 'Д';
+//        answer[1] = 'о';
+//        answer[2] = 'х';
+//        answer[3] = 'а';
+    }
+}
