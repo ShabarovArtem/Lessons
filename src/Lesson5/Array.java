d4f249de8b734e23fc534d171b40b6cc03f3d7df
ShabarovArtem <shabarovartemy@yandex.ru>
2024-02-10 23:27:30 +0300

    Init

---
+++ src/Lesson5/Array.java
@@ -0,0 +1,17 @@
+package Lesson5;
+
+import java.util.Arrays;
+
+public class Array {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+  //      int[] array = new int[2];
+  //      System.out.println(Arrays.toString(array));
+ //       array[0] = x;
+//        int [] array = new int[]{x,y};
+        int [] array = {x,y};
+        System.out.println(Arrays.toString(array));
+        System.out.println(array[1]);
+    }
+}
