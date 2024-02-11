d0301009e5cf5cebb23b6156224efbe125b530e4
ShabarovArtem <shabarovartemy@yandex.ru>
2024-02-11 23:27:42 +0300

    Init

--- src/Practica/Razbieniemasiva.java
+++ src/Practica/Razbieniemasiva.java
@@ -5,10 +5,12 @@ import java.util.Arrays;
 public class Razbieniemasiva {
     public static void main(String[] args) {
         String text = "my name is Giovanni Giorgio";
-        String substringName = text.substring(11, text.length()).toLowerCase();
+        String substringName = text.substring(11).toLowerCase();
         String[] name = substringName.split(" ");
-        System.out.println(Arrays.toString(name));
-        System.out.println(name.length);
+        for (String world : name){
+            System.out.println(world);
+            System.out.println(world.length());
+        }
 
 
 
