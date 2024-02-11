e9974e79b56434bf38e72412e498c3d07c878e9a
ShabarovArtem <shabarovartemy@yandex.ru>
2024-02-11 13:44:05 +0300

    Init

--- src/Practica/Sortirovka.java
+++ src/Practica/Sortirovka.java
@@ -2,11 +2,26 @@ package Practica;
 
 public class Sortirovka {
     public static void main(String[] args) {
-        int[] numbers2 = {3,2,10,5,8};
-            for (int i =0; i<numbers2.length;i++){
-                if(!(numbers2[i] % 2 > 0)){
+        int[] numbers2 = {3, 2, 10, 5, 8};
+//        1 задание
+//
+////            for (int i =0; i<numbers2.length;i++){
+////                if(!(numbers2[i] % 2 > 0)){
+////                    System.out.println(numbers2[i]);
+////                }
+////            }
+//        for (int number : numbers2) {
+//                if(number % 2 == 0){
+//                    System.out.println(number);
+//                }
+//            }
+//
+//        }
+//        2 задание
+        for (int i =0; i<numbers2.length;i++) {
+                if(i % 2 == 0){
                     System.out.println(numbers2[i]);
                 }
             }
+        }
     }
-}
